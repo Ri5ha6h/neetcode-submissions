67734df1c1f60b21b6d2aacb85d9198e12b60ad5
Rishabh Malik <76163287+Ri5ha6h@users.noreply.github.com>
2026-05-21 16:30:17 +0530

    Add: replace-elements-with-greatest-element-on-right-side - submission-1

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+    replaceElements(arr) {
+        const len = arr.length
+        const narr = new Array(len).fill(-1)
+        for(let i = 0; i < len - 1; i++){
+            let max = 0;
+            for(let j = i + 1; j < len; j++){
+                if(arr[j] > max){
+                    max = arr[j]
+                }
+            }
+            narr[i] = max;
+        }
+        //console.log(narr)
+        return narr
+    }
+}
